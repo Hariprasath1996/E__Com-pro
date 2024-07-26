1184f4adbd241373c2215cdfcc1b8ca615a939da
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-26 17:25:25 +0530

    navbar created

--- E-Commerce Proj/src/Components/Navbar/Navbar.jsx
+++ E-Commerce Proj/src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { GiTigerHead } from "react-icons/gi";
 import "../Navbar.css"
 const Navbar = () => {
     return (
         <nav className='Container'>
             <div className='Header'>
-                <div className='heading'> <span style={{ color:'orangered'}}>SKELE</span>TON <span ><GiTigerHead style={{  color:'orangered'}} /></span></div>
+                <div className='heading'> <span style={{ color: 'orangered' }}>SKELE</span>TON <span ><GiTigerHead style={{ color: 'orangered' }} /></span></div>
                 <div className='header-Options'>
                     <ul>
-                        <li>CATAGORIES</li>
-                        <li>ABOUT</li>
-                        <li>CONTACT</li>
-                        <li>SIGN_UP</li>
-                        <li>CART</li>
+                        <Link><li>CATAGORIES</li></Link>
+                        <Link> <li>ABOUT</li></Link>
+                        <Link><li>CONTACT</li></Link>
+                        <Link><li>SIGN_UP</li></Link>
+                        <Link><li>CART</li></Link>
                     </ul>
                 </div>
             </div>
