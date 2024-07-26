9c726a5f179c3d7be6207a30f165ace5935ff233
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-26 14:32:04 +0530

    created nav bar

---
+++ E-Commerce Proj/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='Container'>
+            <div className='Header'>
+                <div className='heading'>GIFT_SHOP</div>
+                <div className='header-Options'>
+                    <ul>
+                        <li>CATAGORIES</li>
+                        <li>ABOUT</li>
+                        <li>CONTACT</li>
+                        <li>SIGN_UP</li>
+                        <li>CART</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
