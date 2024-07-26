3191d47dc7bc5b4c97b871059fad5bbd9c2226ca
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-26 15:20:54 +0530

    created nav bar

--- E-Commerce Proj/src/App.jsx
+++ E-Commerce Proj/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "./App.css"
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      {/* <Route  path="/" element={<Home cart={cart} setCart={setCart} /> }></Route> */}
+    </Routes>
+    </BrowserRouter>
   )
 }
 export default App
--- E-Commerce Proj/src/Components/Navbar/Navbar.jsx
+++ E-Commerce Proj/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { GiTigerHead } from "react-icons/gi";
+import "../Navbar.css"
 const Navbar = () => {
     return (
         <nav className='Container'>
             <div className='Header'>
-                <div className='heading'>GIFT_SHOP</div>
+                <div className='heading'> <span style={{ color:'orangered'}}>SKELE</span>TON <span ><GiTigerHead style={{  color:'orangered'}} /></span></div>
                 <div className='header-Options'>
                     <ul>
                         <li>CATAGORIES</li>
