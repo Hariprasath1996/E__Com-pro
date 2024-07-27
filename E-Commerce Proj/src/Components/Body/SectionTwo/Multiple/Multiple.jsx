94bcc256cc9ed80cdc0cd00ce5a85eeef7db8706
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-27 19:37:25 +0530

    cloth section completed

--- E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
+++ E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
@@ -6,72 +6,86 @@ const Multiple = () => {
             <div className='multi'>
                 {/* ...electronic section... */}
                 <div className='Ele'>
-                <h1>Best Collections From Electronics:</h1>
-                <div className='Ele-col'>
-                <div className='head-set'>
-                        <img src="https://i.pinimg.com/564x/ca/b0/7d/cab07d006fcb3b7009d1ef12bb9c4625.jpg" alt="airPods" />
-                        <p>Best true Wireless headphones...</p>
-                        <h2>From <span>RS.</span>1,399</h2>
+                    <h1>Best Collections From Electronics :</h1>
+                    <div className='Ele-col'>
+                        <div className='head-set'>
+                            <img src="https://i.pinimg.com/564x/ca/b0/7d/cab07d006fcb3b7009d1ef12bb9c4625.jpg" alt="#" />
+                            <p>Best true Wireless headphones...</p>
+                            <h2>From <span>RS.</span>1,399</h2>
+                        </div>
+                        <div className='Mobiles'>
+                            <img src="https://i.pinimg.com/564x/6b/bf/ff/6bbfff73277fcf4c83ad2ce49a6ee972.jpg" alt="#" />
+                            <p>Best Branded Mobiles...</p>
+                            <h2>From <span>RS.</span>7,499</h2>
+                        </div>
+                        <div className='Television'>
+                            <img src="https://i.pinimg.com/564x/8d/df/d8/8ddfd8b3287cfdbab8819e859d1cd4eb.jpg" alt="#" />
+                            <p>Best Company Television...</p>
+                            <h2>From <span>RS.</span>9,299</h2>
+                        </div>
+                        <div className='Laptop'>
+                            <img src="https://i.pinimg.com/564x/76/36/5c/76365c73c4b4c00f470645ea33924adb.jpg" alt="#" />
+                            <p>Best  professional  laptops...</p>
+                            <h2>From <span>RS.</span>8,299</h2>
+                        </div>
+                        <div className='Monitor'>
+                            <img src="https://i.pinimg.com/564x/f5/0e/ef/f50eef573f4af7bbfbc8d2d0c304b5fd.jpg" alt="#" />
+                            <p>Best Gaming Monitors...</p>
+                            <h2>From <span>RS.</span>6,499</h2>
+                        </div>
                     </div>
-                    <div className='Mobiles'>
-                    <img src="https://i.pinimg.com/564x/6b/bf/ff/6bbfff73277fcf4c83ad2ce49a6ee972.jpg" alt="airPods" />
-                        <p>Best Mobiles...</p>
-                        <h2>From <span>RS.</span>7,499</h2> 
-                    </div>
-                    <div className='Television'>
-                    <img src="https://i.pinimg.com/564x/8d/df/d8/8ddfd8b3287cfdbab8819e859d1cd4eb.jpg" alt="airPods" />
-                        <p>Best Television...</p>
-                        <h2>From <span>RS.</span>9,299</h2> 
-                    </div>
-                    <div className='Laptop'>
-                    <img src="https://i.pinimg.com/564x/76/36/5c/76365c73c4b4c00f470645ea33924adb.jpg" alt="airPods" />
-                        <p>Best laptops...</p>
-                        <h2>From <span>RS.</span>8,299</h2> 
-                    </div>
-                    <div className='Monitor'>
-                    <img src="https://i.pinimg.com/564x/f5/0e/ef/f50eef573f4af7bbfbc8d2d0c304b5fd.jpg" alt="airPods" />
-                        <p>Best Monitors...</p>
-                        <h2>From <span>RS.</span>8,299</h2> 
-                    </div>
-                </div>
-                    
                 </div>
                 {/* ...toy section... */}
                 <div className='toy'>
-                <h1>Best Collections From Toys:</h1>
-                <div className='toy-col'>
-                <div className='Movie-Toys'>
-                        <img src="https://i.pinimg.com/564x/b4/de/14/b4de142ecd12c0fcfa7e122849036fa4.jpg" alt="airPods" />
-                        <p>Best Movie-Toys collections here ...</p>
-                        <h2>From <span>RS.</span>1,399</h2>
+                    <h1>Best Collections From Toys:</h1>
+                    <div className='toy-col'>
+                        <div className='Movie-Toys'>
+                            <img src="https://i.pinimg.com/564x/b4/de/14/b4de142ecd12c0fcfa7e122849036fa4.jpg" alt="#" />
+                            <p>Best Movie-Toys collections here ...</p>
+                            <h2>From <span>RS.</span>1,399</h2>
+                        </div>
+                        <div className='Robotic-Toys'>
+                            <img src="https://i.pinimg.com/564x/61/3f/3b/613f3b10856aa0e89c3210ce6d1c782c.jpg" alt="#" />
+                            <p>Best Robotic-Toys available...</p>
+                            <h2>From <span>RS.</span>6,499</h2>
+                        </div>
+                        <div className='Teddy'>
+                            <img src="https://i.pinimg.com/564x/38/a9/9e/38a99ef27007cfae409d65c0b935c9d8.jpg" alt="#" />
+                            <p>Best Teddy Collections present ...</p>
+                            <h2>From <span>RS.</span>1,299</h2>
+                        </div>
+                        <div className='Wireless-toys'>
+                            <img src="https://i.pinimg.com/564x/ce/ca/e2/cecae21dbd54e27d7b27240a3300c8ce.jpg" alt="#" />
+                            <p>Best Wireless-toys available...</p>
+                            <h2>From <span>RS.</span>5,299</h2>
+                        </div>
+                        <div className='Wireless-toys'>
+                            <img src="https://i.pinimg.com/564x/2b/54/7a/2b547a606ecd67a922a4e729975f5bf5.jpg" alt="#" />
+                            <p>Best plastic-toys available...</p>
+                            <h2>From <span>RS.</span>4,199</h2>
+                        </div>
                     </div>
-                    <div className='Robotic-Toys'>
-                    <img src="https://i.pinimg.com/564x/61/3f/3b/613f3b10856aa0e89c3210ce6d1c782c.jpg" alt="airPods" />
-                        <p>Best Robotic-Toys available...</p>
-                        <h2>From <span>RS.</span>6,499</h2> 
-                    </div>
-                    <div className='Teddy'>
-                    <img src="https://i.pinimg.com/564x/38/a9/9e/38a99ef27007cfae409d65c0b935c9d8.jpg" alt="airPods" />
-                        <p>Best Teddy Collections present ...</p>
-                        <h2>From <span>RS.</span>1,299</h2> 
-                    </div>
-                    <div className='Wireless-toys'>
-                    <img src="https://i.pinimg.com/564x/ce/ca/e2/cecae21dbd54e27d7b27240a3300c8ce.jpg" alt="airPods" />
-                        <p>Best Wireless-toys available...</p>
-                        <h2>From <span>RS.</span>5,299</h2> 
-                    </div>
-                    <div className='Wireless-toys'>
-                    <img src="https://i.pinimg.com/564x/2b/54/7a/2b547a606ecd67a922a4e729975f5bf5.jpg" alt="airPods" />
-                        <p>Best plastic-toys available...</p>
-                        <h2>From <span>RS.</span>5,299</h2> 
-                    </div>
-                    
                 </div>
+                <div className='Cloths'>
+                    <h1>Best Collection From Cloths :</h1>
+                    <div className='Both-collections'>
+                    <div className='For-Kids'>
+                            <img src="https://i.pinimg.com/564x/7b/0f/d7/7b0fd7264cab9b3dc4005f396c2fa3f6.jpg" alt="#" />
+                            <p>Best Costumes for Men  available here ...</p>
+                            <h2>From <span>RS.</span>499</h2>
+                        </div>
+                        <div className='For-Mens'>
+                            <img src="https://i.pinimg.com/564x/0e/90/a5/0e90a53ee2a4fe25f1e739362c19d329.jpg" alt="#" />
+                            <p>Best Costumes for Men  available here ...</p>
+                            <h2>From <span>RS.</span>399</h2>
+                        </div>
+                        <div className='For-Women'>
+                            <img src="https://i.pinimg.com/564x/fe/bd/7f/febd7f63f11adb997c5e115048911d58.jpg" alt="#" />
+                            <p>Best Costumes for Women  available here ...</p>
+                            <h2>From <span>RS.</span>1,199</h2>
+                        </div>
+                    </div>
                 </div>
-                {/* ........ */}
-                <div className='Clo'></div>
-                {/* ........ */}
-                <div className='foo'></div>
             </div>
         </>
     )
