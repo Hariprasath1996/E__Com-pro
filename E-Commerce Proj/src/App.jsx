6b7cc5aa451154bd7cca145306545f4f0f1b3128
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-27 14:37:19 +0530

    3rd section completed

--- E-Commerce Proj/src/App.jsx
+++ E-Commerce Proj/src/App.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import "./App.css"
 import Header from './Components/Header/Header'
 import SubHeading from './Components/Body/SectionOne/SubHeading'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-
+import Multiple from './Components/Body/SectionTwo/Multiple/Multiple'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
   return (
-    <BrowserRouter>
-    <Header/>
-    
-    <Routes>
-      {/* <Route  path="" element={<SectionOne  /> }></Route> */}
-    </Routes>
-    <SubHeading />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <SubHeading />
+        <Multiple />
+        <Routes>
+          {/* <Route  path="/" element={ }></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+
   )
 }
 export default App
---
+++ E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Multiple.css"
+const Multiple = () => {
+    return (
+        <>
+            <div className='multi'>
+                <div className='Ele'>
+                    <div>
+                        <img src="" alt="" />
+                        <p></p>
+                        <h2>From <span>\u20A8</span>1,499</h2>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className='toy'></div>
+                <div className='Clo'></div>
+                <div className='foo'></div>
+
+            </div>
+        </>
+    )
+}
+
+export default Multiple
