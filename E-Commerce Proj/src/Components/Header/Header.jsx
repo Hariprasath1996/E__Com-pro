b45da4864642b7cdfe150e88287338764257fc3c
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-27 15:13:47 +0530

    changes added

--- E-Commerce Proj/src/Components/Header/Header.jsx
+++ E-Commerce Proj/src/Components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { GiTigerHead } from "react-icons/gi";
 import { FaShoppingCart } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa";
 import { RiAccountPinCircleFill } from "react-icons/ri";
 import "./Header.css"
 const Header = () => {
@@ -11,15 +12,15 @@ const Header = () => {
                 <div className='heading'> <span style={{ color: 'orangered' }}>HUNT</span>ER<span ><GiTigerHead style={{ color: 'orangered' }} /></span></div>
                 <div className='header-Options'>
                     <ul>
-                        <Link className='collection-head'>COLLECTIONS
-                            {/* <div className='collections'>
+                        <Link className='collection-head'>COLLECTIONS <FaAngleDown className='FaAngleDown' style={{ color: 'orangered' }} />
+                            <div className='collections'>
                                 <ul className='sub-items'>
                                     <Link>Electronics</Link>
                                     <Link>Toys</Link>
                                     <Link>Cloths</Link>
                                     <Link>Foot_Wear</Link>
                                 </ul>
-                            </div> */}
+                            </div>
                         </Link>
                         <Link>ABOUT</Link>
                         <Link > <span style={{ fontSize: 20 }}><RiAccountPinCircleFill /></span> LOG_<span style={{ color: 'orangered' }}>IN</span> </Link>
