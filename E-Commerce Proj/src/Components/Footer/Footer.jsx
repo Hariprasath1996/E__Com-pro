b34eff822cdc637b1a3d0fd625238a0b353f6f81
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-28 12:24:19 +0530

    adding some css features

--- E-Commerce Proj/src/Components/Footer/Footer.jsx
+++ E-Commerce Proj/src/Components/Footer/Footer.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import "./Footer.css"
+import { FaWhatsapp } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { MdContactPhone } from "react-icons/md";
+
 const Footer = () => {
     return (
         <>
-            <div>
-                <div>About</div>
-                <div>Contact_us</div>
-                <div>Help</div>
-                <div>Social</div>
+            <div className='footer-sec'>
+                <div>About
+                    <div>
+                        FAQ
+                    </div>
+                </div>
+                <div>Contact_us
+                    <div className='sub-con'>
+                        <FaWhatsapp />
+                        <FaTelegram />
+                        <SiGmail />
+                    </div>
+                </div>
+                <div>Help
+                    <div>
+                    <MdContactPhone />
+                    </div>
+                </div>
+                <div>Social
+                    <div className='sub-so'>
+                        <FaWhatsapp />
+                        <FaInstagram />
+                        <FaXTwitter />
+                        <FaFacebook />
+                    </div>
+                </div>
             </div>
         </>
     )
