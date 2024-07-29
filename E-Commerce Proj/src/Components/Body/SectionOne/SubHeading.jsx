b37f652d2205d54b491a491a147aba792a96fe30
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-29 11:06:49 +0530

    added some css styles to modified ui

--- E-Commerce Proj/src/Components/Body/SectionOne/SubHeading.jsx
+++ E-Commerce Proj/src/Components/Body/SectionOne/SubHeading.jsx
@@ -7,39 +7,39 @@ const SubHeading = () => {
             <div className='subContainer'>
                 <ul className='sub-ul'>
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/564x/fd/56/fe/fd56fe106a889d7842bd4e519a95d5c8.jpg" alt="Home_Appliances" />
                         <Link>
-                            Electronics
+                            <img src="https://i.pinimg.com/564x/fd/56/fe/fd56fe106a889d7842bd4e519a95d5c8.jpg" alt="Home_Appliances" />
+                            <p className='para'>Electronics</p>
                         </Link>
                     </li>
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/736x/e9/0f/ad/e90fadde9d55992ab9bc88da48595d19.jpg" alt="tos" />
                         <Link>
-                            Toys
+                            <img src="https://i.pinimg.com/736x/e9/0f/ad/e90fadde9d55992ab9bc88da48595d19.jpg" alt="tos" />
+                            <p className='para'>Toys</p>
                         </Link>
                     </li>
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/564x/f9/2a/59/f92a59e0435fde6ee1a5efe3d04620e4.jpg" alt="Cloths" />
                         <Link>
-                            Cloths
+                            <img src="https://i.pinimg.com/564x/f9/2a/59/f92a59e0435fde6ee1a5efe3d04620e4.jpg" alt="Cloths" />
+                            <p className='para'>Cloths</p>
                         </Link>
                     </li >
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/736x/b1/6d/cc/b16dccc234c87341d2cec7e8dccf11c4.jpg" alt="Foot_Wear" />
                         <Link>
-                            Foot_Wear
+                            <img src="https://i.pinimg.com/736x/b1/6d/cc/b16dccc234c87341d2cec7e8dccf11c4.jpg" alt="Foot_Wear" />
+                            <p className='para'>Foot_Wear</p>
                         </Link>
                     </li>
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/564x/33/fe/86/33fe8689149934ea6d63f6f879955b8a.jpg" alt="Foot_Wear" />
                         <Link>
-                            Groceries
+                            <img src="https://i.pinimg.com/564x/33/fe/86/33fe8689149934ea6d63f6f879955b8a.jpg" alt="Foot_Wear" />
+                            <p className='para'>Groceries</p>
                         </Link>
                     </li>
                     <li className='sub-list'>
-                        <img src="https://i.pinimg.com/564x/2a/a1/26/2aa126327ce22bee466e2744f837d124.jpg" />
                         <Link>
-                            Stationeries
+                            <img src="https://i.pinimg.com/564x/2a/a1/26/2aa126327ce22bee466e2744f837d124.jpg" />
+                            <p className='para'>Stationeries</p>
                         </Link>
                     </li>
                 </ul>
--- E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
+++ E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Multiple.css"
+import { Link } from 'react-router-dom'
 const Multiple = () => {
     return (
         <>
@@ -8,95 +9,95 @@ const Multiple = () => {
                 <div className='Ele'>
                     <h1>Best Collections From Electronics :</h1>
                     <div className='Ele-col'>
-                        <div className='head-set'>
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Headset Collections</p>
+                        <Link className='head-set'>
+                            <p  style={{fontSize:20 , fontWeight:"bold"}}>Headset Collections</p>
                             <img src="https://i.pinimg.com/564x/ca/b0/7d/cab07d006fcb3b7009d1ef12bb9c4625.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}> Wireless headphones...</p>
-                            <h2>From <span>RS.</span>1,399</h2>
-                        </div>
-                        <div className='Mobiles'>
+                            <h2>From <span>₹ </span>1,399</h2>
+                        </Link>
+                        <Link className='Mobiles'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Mobile Collections</p>
                             <img src="https://i.pinimg.com/564x/6b/bf/ff/6bbfff73277fcf4c83ad2ce49a6ee972.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}> Branded Mobiles...</p>
-                            <h2>From <span>RS.</span>7,499</h2>
-                        </div>
-                        <div className='Television'>
+                            <h2>From <span>₹ </span>7,499</h2>
+                        </Link>
+                        <Link className='Television'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Television Collections</p>
                             <img src="https://i.pinimg.com/564x/8d/df/d8/8ddfd8b3287cfdbab8819e859d1cd4eb.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}> Company Television...</p>
-                            <h2>From <span>RS.</span>9,299</h2>
-                        </div>
-                        <div className='Laptop'>
+                            <h2>From <span>₹ </span>9,299</h2>
+                        </Link>
+                        <Link className='Laptop'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Laptop Collections</p>
                             <img src="https://i.pinimg.com/564x/76/36/5c/76365c73c4b4c00f470645ea33924adb.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>  professional  laptops...</p>
-                            <h2>From <span>RS.</span>8,299</h2>
-                        </div>
-                        <div className='Monitor'>
+                            <h2>From <span>₹ </span>8,299</h2>
+                        </Link>
+                        <Link className='Monitor'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Monitor Collections</p>
                             <img src="https://i.pinimg.com/564x/f5/0e/ef/f50eef573f4af7bbfbc8d2d0c304b5fd.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}> Gaming Monitors...</p>
-                            <h2>From <span>RS.</span>6,499</h2>
-                        </div>
+                            <h2>From <span>₹ </span>6,499</h2>
+                        </Link>
                     </div>
                 </div>
                 {/* ...toy section... */}
                 <div className='toy'>
                     <h1>Best Collections From Toys:</h1>
                     <div className='toy-col'>
-                        <div className='Movie-Toys'>
+                        <Link className='Movie-Toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Movie Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/b4/de/14/b4de142ecd12c0fcfa7e122849036fa4.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}> Movie-Toys collections </p>
-                            <h2>From <span>RS.</span>1,399</h2>
-                        </div>
-                        <div className='Robotic-Toys'>
+                            <h2>From <span>₹ </span>1,399</h2>
+                        </Link>
+                        <Link className='Robotic-Toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Robotic-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/61/3f/3b/613f3b10856aa0e89c3210ce6d1c782c.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Robotic-Toys </p>
-                            <h2>From <span>RS.</span>6,499</h2>
-                        </div>
-                        <div className='Teddy'>
+                            <h2>From <span>₹ </span>6,499</h2>
+                        </Link>
+                        <Link className='Teddy'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Teddy Collections</p>
                             <img src="https://i.pinimg.com/564x/38/a9/9e/38a99ef27007cfae409d65c0b935c9d8.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Teddy Collections </p>
-                            <h2>From <span>RS.</span>1,299</h2>
-                        </div>
-                        <div className='Wireless-toys'>
+                            <h2>From <span>₹ </span>1,299</h2>
+                        </Link>
+                        <Link className='Wireless-toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Remote-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/ce/ca/e2/cecae21dbd54e27d7b27240a3300c8ce.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Wireless-toys</p>
-                            <h2>From <span>RS.</span>5,299</h2>
-                        </div>
-                        <div className='plastic-toys'>
+                            <h2>From <span>₹ </span>5,299</h2>
+                        </Link>
+                        <Link className='plastic-toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Baby-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/2b/54/7a/2b547a606ecd67a922a4e729975f5bf5.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best plastic-toys </p>
-                            <h2>From <span>RS.</span>4,199</h2>
-                        </div>
+                            <h2>From <span>₹ </span>4,199</h2>
+                        </Link>
                     </div>
                 </div>
                 <div className='Cloths'>
                     <h1>Best Collection From Cloths :</h1>
                     <div className='Both-collections'>
-                        <div className='For-Kids'>
+                        <Link className='For-Kids'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Kids Collections</p>
                             <img src="https://i.pinimg.com/564x/7b/0f/d7/7b0fd7264cab9b3dc4005f396c2fa3f6.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Kids  available here ...</p>
-                            <h2>From <span>RS.</span>499</h2>
-                        </div>
-                        <div className='For-Mens'>
+                            <h2>From <span>₹ </span>499</h2>
+                        </Link>
+                        <Link className='For-Mens'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Mens Collections</p>
                             <img src="https://i.pinimg.com/564x/0e/90/a5/0e90a53ee2a4fe25f1e739362c19d329.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Mens  available here ...</p>
-                            <h2>From <span>RS.</span>399</h2>
-                        </div>
-                        <div className='For-Women'>
+                            <h2>From <span>₹ </span>399</h2>
+                        </Link>
+                        <Link className='For-Women'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Women Collections</p>
                             <img src="https://i.pinimg.com/564x/fe/bd/7f/febd7f63f11adb997c5e115048911d58.jpg" alt="#" />
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for women's  available here ...</p>
-                            <h2>From <span>RS.</span>1,199</h2>
-                        </div>
+                            <h2>From <span>₹ </span>1,199</h2>
+                        </Link>
                     </div>
                 </div>
             </div>
