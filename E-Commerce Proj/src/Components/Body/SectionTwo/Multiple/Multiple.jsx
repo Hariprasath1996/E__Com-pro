4bbd8af0ede6be93b63ec7e42a2aad804f131b83
HariPrasath1996 <hasathphy96@gmail.com>
2024-07-29 10:34:10 +0530

    modified css styles

--- E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
+++ E-Commerce Proj/src/Components/Body/SectionTwo/Multiple/Multiple.jsx
@@ -17,25 +17,25 @@ const Multiple = () => {
                         <div className='Mobiles'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Mobile Collections</p>
                             <img src="https://i.pinimg.com/564x/6b/bf/ff/6bbfff73277fcf4c83ad2ce49a6ee972.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Branded Mobiles...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}> Branded Mobiles...</p>
                             <h2>From <span>RS.</span>7,499</h2>
                         </div>
                         <div className='Television'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Television Collections</p>
                             <img src="https://i.pinimg.com/564x/8d/df/d8/8ddfd8b3287cfdbab8819e859d1cd4eb.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Company Television...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}> Company Television...</p>
                             <h2>From <span>RS.</span>9,299</h2>
                         </div>
                         <div className='Laptop'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Laptop Collections</p>
                             <img src="https://i.pinimg.com/564x/76/36/5c/76365c73c4b4c00f470645ea33924adb.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best  professional  laptops...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>  professional  laptops...</p>
                             <h2>From <span>RS.</span>8,299</h2>
                         </div>
                         <div className='Monitor'>
                             <p style={{fontSize:20 , fontWeight:"bold"}}>Monitor Collections</p>
                             <img src="https://i.pinimg.com/564x/f5/0e/ef/f50eef573f4af7bbfbc8d2d0c304b5fd.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Gaming Monitors...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}> Gaming Monitors...</p>
                             <h2>From <span>RS.</span>6,499</h2>
                         </div>
                     </div>
@@ -47,31 +47,31 @@ const Multiple = () => {
                         <div className='Movie-Toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Movie Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/b4/de/14/b4de142ecd12c0fcfa7e122849036fa4.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}> Movie-Toys collections here ...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}> Movie-Toys collections </p>
                             <h2>From <span>RS.</span>1,399</h2>
                         </div>
                         <div className='Robotic-Toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Robotic-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/61/3f/3b/613f3b10856aa0e89c3210ce6d1c782c.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Robotic-Toys available...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Robotic-Toys </p>
                             <h2>From <span>RS.</span>6,499</h2>
                         </div>
                         <div className='Teddy'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Teddy Collections</p>
                             <img src="https://i.pinimg.com/564x/38/a9/9e/38a99ef27007cfae409d65c0b935c9d8.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Teddy Collections present ...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Teddy Collections </p>
                             <h2>From <span>RS.</span>1,299</h2>
                         </div>
                         <div className='Wireless-toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Remote-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/ce/ca/e2/cecae21dbd54e27d7b27240a3300c8ce.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Wireless-toys available...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Wireless-toys</p>
                             <h2>From <span>RS.</span>5,299</h2>
                         </div>
                         <div className='plastic-toys'>
                         <p style={{fontSize:20 , fontWeight:"bold"}}>Baby-Toys Collections</p>
                             <img src="https://i.pinimg.com/564x/2b/54/7a/2b547a606ecd67a922a4e729975f5bf5.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best plastic-toys available...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best plastic-toys </p>
                             <h2>From <span>RS.</span>4,199</h2>
                         </div>
                     </div>
@@ -80,21 +80,21 @@ const Multiple = () => {
                     <h1>Best Collection From Cloths :</h1>
                     <div className='Both-collections'>
                         <div className='For-Kids'>
-                        <p style={{fontSize:20 , fontWeight:"bold"}}>Kids-Costumes Collections</p>
+                        <p style={{fontSize:20 , fontWeight:"bold"}}>Kids Collections</p>
                             <img src="https://i.pinimg.com/564x/7b/0f/d7/7b0fd7264cab9b3dc4005f396c2fa3f6.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Men  available here ...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Kids  available here ...</p>
                             <h2>From <span>RS.</span>499</h2>
                         </div>
                         <div className='For-Mens'>
-                        <p style={{fontSize:20 , fontWeight:"bold"}}>Mens-Costumes Collections</p>
+                        <p style={{fontSize:20 , fontWeight:"bold"}}>Mens Collections</p>
                             <img src="https://i.pinimg.com/564x/0e/90/a5/0e90a53ee2a4fe25f1e739362c19d329.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Men  available here ...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Mens  available here ...</p>
                             <h2>From <span>RS.</span>399</h2>
                         </div>
                         <div className='For-Women'>
-                        <p style={{fontSize:20 , fontWeight:"bold"}}>Women-Costumes Collections</p>
+                        <p style={{fontSize:20 , fontWeight:"bold"}}>Women Collections</p>
                             <img src="https://i.pinimg.com/564x/fe/bd/7f/febd7f63f11adb997c5e115048911d58.jpg" alt="#" />
-                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for Women  available here ...</p>
+                            <p style={{fontSize:20 , fontWeight:"bold"}}>Best Costumes for women's  available here ...</p>
                             <h2>From <span>RS.</span>1,199</h2>
                         </div>
                     </div>
